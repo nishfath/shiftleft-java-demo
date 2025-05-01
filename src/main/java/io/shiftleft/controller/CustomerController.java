3a740851af343344eacb7b5735fb30d82550e28f
nishfath <81247855+nishfath@users.noreply.github.com>
2025-05-01 13:19:19 -0400

    Fixing src/main/java/io/shiftleft/controller/CustomerController.java for finding 4

--- src/main/java/io/shiftleft/controller/CustomerController.java
+++ src/main/java/io/shiftleft/controller/CustomerController.java
@@ -277,7 +277,7 @@ public class CustomerController {
    * @return String
    * @throws IOException
    */
-  @RequestMapping(value = "/debug", method = RequestMethod.GET)
+@RequestMapping(value = "/debug", method = RequestMethod.GET)
   public String debug(@RequestParam String customerId,
 					  @RequestParam int clientId,
 					  @RequestParam String firstName,
@@ -303,8 +303,10 @@ public class CustomerController {
     httpResponse.setHeader("Location", String.format("%s/customers/%s",
                            request.getContextPath(), customer1.getId()));
 
-    return customer1.toString().toLowerCase().replace("script","");
+    // Properly encode the customer data to prevent XSS
+    return Encode.forHtml(customer1.toString());
   }
+
 
 	/**
 	 * Debug test for saving and reading a customer
